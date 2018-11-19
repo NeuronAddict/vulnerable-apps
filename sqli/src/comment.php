0421e0dba4e77a755e80c234d78af085718804d5
root <root@localhost.localdomain>
2018-11-19 09:37:59 -0500

    add comment with filtered sql.

---
+++ sqli/src/comment.php
@@ -0,0 +1,49 @@
+<h2>filtered injection</h2>
+
+<p>special chars ('";#-) are filtereds, can you inject me?</p>
+<p>note : to see log, set a cookie, get or post log=1</p>
+
+<a href="/comment.php?id=1">/comment.php?id=1</a>
+<?php
+
+function log_($str) {
+	if ($_REQUEST['log']=='1') {
+		echo "<p>$str</p>";	
+	}
+}
+
+function filter($id) {
+	$ret = preg_replace('/[\'";#-]/', '', $id);
+	log_("[*] filter id : \"$id\" ==> \"$ret\"");
+	return $ret;
+}
+
+if(isset($_GET['id'])) {
+
+	$id = filter($_GET['id']);
+
+	$mysqli = mysqli_connect('mysql', 'mysql', 'mysql', 'db');
+	
+	$query = "SELECT id, name, text FROM comments WHERE id=$id";
+
+    log_('[*] "' . print_r($query, true) . '"');
+
+    echo '<br /><br />';
+
+    $res = $mysqli->query($query);
+	
+	if($res) {
+		$res->data_seek(0);
+		while ($row = $res->fetch_assoc()) {
+			echo "<p>comment " . $row['id'] . "</p><h2>" . $row['name'] . "</h2><p>" . $row['text'] . "</p>";
+		}
+    }
+    else {
+        log_( "[-] ". $mysqli->error);
+    }
+
+}
+
+?>
+
+
