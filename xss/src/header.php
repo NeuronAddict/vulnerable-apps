c39801204fe69bd63acaa1c386cdb52d1f831e26
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2018-12-26 17:16:50 +0100

    add headers and login page

---
+++ xss/src/header.php
@@ -0,0 +1,5 @@
+<div>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="reflected.php">reflected</a></li>
+  <li><a href="login.php">login</a></li>
+</div>
---
+++ xss/src/login.php
@@ -0,0 +1,68 @@
+<html>
+
+<body>
+
+    <?php include('header.php'); ?>
+
+    <div>
+    
+        <form method="post">
+            <input type="text" name="login" />
+            <input type="password" name="pass" />
+            <input type="submit" value="Login" />
+        </form>
+    
+    </div>
+
+    <pre>
+        <?php
+
+        $login = ($_REQUEST['login']);
+        $pass = ($_REQUEST['pass']);
+        
+        if($login && $pass) {
+
+            if($_COOKIE['as']) {
+                $login = addslashes($login);
+                $pass = addslashes($pass);
+            }
+
+            $mysqli = mysqli_connect('mysql', 'mysql', 'mysql', 'db');
+
+            $query = "SELECT login, pass FROM user WHERE login = '$login' AND pass = '$pass';";
+
+            // print_r($query);
+
+            echo '<br /><br />';
+
+            $stmt = mysqli_prepare($mysqli, $query);
+            if ($stmt) {
+
+                /* execute statement */
+                mysqli_stmt_execute($stmt);
+
+                /* bind result variables */
+                mysqli_stmt_bind_result($stmt, $login, $pass);
+
+                /* fetch values */
+                while (mysqli_stmt_fetch($stmt)) {
+                    echo "Logged";
+                    //printf ("Logged as %s (%s)\n", $login, $pass);
+                }
+
+                /* close statement */
+                mysqli_stmt_close($stmt);
+            }
+            else {
+                echo mysqli_error ( $mysqli );
+            }
+
+            mysqli_close($mysqli);
+        }
+        ?>
+    </pre>
+
+</body>
+
+</html>
+
