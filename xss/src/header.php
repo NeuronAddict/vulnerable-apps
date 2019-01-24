7120d027bbdd1bde362e871acd9a314e9fce907e
manufrommars <dev@lacourarie.org>
2019-01-24 13:16:33 +0100

    Clickjacking demo.

--- xss/src/header.php
+++ xss/src/header.php
@@ -1,5 +1,9 @@
 <div>
-  <li><a href="index.php">Home</a></li>
-  <li><a href="reflected.php">reflected</a></li>
-  <li><a href="login.php">login</a></li>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="reflected.php">reflected</a></li>
+        <li><a href="login.php">login</a></li>
+        <li><a href="/clickjacking/admin.php">clickjacking : admin</a></li>
+        <li><a href="/clickjacking/index.php">clickjacking : index</a></li>
+    </ul>
 </div>
