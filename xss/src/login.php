93aeb7a09c08aaa3fd9951a5b18cc130920167ae
neuronaddict <dev@lacourarie.org>
2019-09-26 21:59:22 +0200

    Fix login warnings.

--- xss/src/login.php
+++ xss/src/login.php
@@ -1,4 +1,47 @@
-<?php session_start(); ?>
+<?php
+
+session_start();
+
+if( isset($_REQUEST['login']) && $_REQUEST['pass']) {
+    $login = ($_REQUEST['login']);
+    $pass = ($_REQUEST['pass']);
+
+    if ($login && $pass) {
+
+        if ( isset($_COOKIE['as']) && $_COOKIE['as']) {
+            $login = addslashes($login);
+            $pass = addslashes($pass);
+        }
+
+        $mysqli = mysqli_connect('mysql', 'mysql', 'mysql', 'db');
+
+        $query = "SELECT login, pass FROM user WHERE login = '$login' AND pass = '$pass';";
+
+        $stmt = mysqli_prepare($mysqli, $query);
+        if ($stmt) {
+
+            /* execute statement */
+            mysqli_stmt_execute($stmt);
+
+            /* bind result variables */
+            mysqli_stmt_bind_result($stmt, $login, $pass);
+
+            /* fetch values */
+            while (mysqli_stmt_fetch($stmt)) {
+                $_SESSION['logged'] = true;
+
+            }
+
+            /* close statement */
+            mysqli_stmt_close($stmt);
+        } else {
+            echo mysqli_error($mysqli);
+        }
+
+        mysqli_close($mysqli);
+    }
+}
+?>
 <html>
 
 <body>
@@ -14,55 +57,6 @@
         </form>
     
     </div>
-
-    <pre>
-        <?php
-
-        $login = ($_REQUEST['login']);
-        $pass = ($_REQUEST['pass']);
-        
-        if($login && $pass) {
-
-            if($_COOKIE['as']) {
-                $login = addslashes($login);
-                $pass = addslashes($pass);
-            }
-
-            $mysqli = mysqli_connect('mysql', 'mysql', 'mysql', 'db');
-
-            $query = "SELECT login, pass FROM user WHERE login = '$login' AND pass = '$pass';";
-
-            // print_r($query);
-
-            echo '<br /><br />';
-
-            $stmt = mysqli_prepare($mysqli, $query);
-            if ($stmt) {
-
-                /* execute statement */
-                mysqli_stmt_execute($stmt);
-
-                /* bind result variables */
-                mysqli_stmt_bind_result($stmt, $login, $pass);
-
-                /* fetch values */
-                while (mysqli_stmt_fetch($stmt)) {
-                    echo "Logged";
-                    $_SESSION['logged'] = true;
-                    //printf ("Logged as %s (%s)\n", $login, $pass);
-                }
-
-                /* close statement */
-                mysqli_stmt_close($stmt);
-            }
-            else {
-                echo mysqli_error ( $mysqli );
-            }
-
-            mysqli_close($mysqli);
-        }
-        ?>
-    </pre>
 
 </body>
 
