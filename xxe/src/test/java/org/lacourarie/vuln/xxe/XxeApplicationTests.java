0b37004a5982c65d288d6cc5b4baf3e66e929d70
manufrommars <dev@lacourarie.org>
2019-11-16 15:00:17 +0100

    XXE vuln app : initial commit.

---
+++ xxe/src/test/java/org/lacourarie/vuln/xxe/XxeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lacourarie.vuln.xxe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
